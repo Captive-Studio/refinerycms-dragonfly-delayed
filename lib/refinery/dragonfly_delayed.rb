cd716e373b9e7b20203ee8b9e5b32633c03371b5
Stéphane Hanser <shanser@captive.fr>
2019-02-01 12:43:43 +0100

    gère le FileDatastore temps: 0

--- lib/refinery/dragonfly_delayed.rb
+++ lib/refinery/dragonfly_delayed.rb
@@ -10,23 +10,27 @@ module Refinery
 
   module DragonflyDelayed
     require 'refinery/dragonfly_delayed/engine'
-    
-    ::Dragonfly.app(:refinery_images).configure do
+    images_app = ::Dragonfly.app(:refinery_images)
+
+    images_app.configure do
       before_serve do |job, env|
         already_generated_thumb = Thumb.where(signature: job.serialize, generated: true).first
         location = if already_generated_thumb.present?
-          ::Dragonfly.app(:refinery_images).datastore.url_for(already_generated_thumb.uid)
+          images_app.datastore.url_for(already_generated_thumb.uid)
         else
           fetch = job.steps.select {|j| j.is_a? ::Dragonfly::Job::Fetch}.first
           original_image = Refinery::Image.where(image_uid: fetch.uid).first
           throw :halt, [ 404, {}, [] ] if original_image.blank?
 
           ImageResizer.register_job_to_process job
-          original_image.image.remote_url
         end
         throw :halt, [ 302, {'Location' => location}, [] ]
       end
     end
     
+    if images_app.datastore.is_a? ::Dragonfly::FileDataStore
+      images_app.datastore.server_root = 'public'
+    end
+
   end
 end
